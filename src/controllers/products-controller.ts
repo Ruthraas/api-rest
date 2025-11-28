576694a38d27ef4e1e60c57416fb93ed8bdd7e45
Arthur <arthur@example.com>
2025-11-28 13:44:49 -0300

    api-rest

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
-import { AppError } from "../utils/app-Error";
+import { AppError } from "../utils/app-error";
 
 class ProductsController {
   index(req: Request, res: Response) {
@@ -10,21 +11,31 @@ class ProductsController {
   }
 
   create(req: Request, res: Response) {
-    const { name, price } = req.body;
+    const bodySchema = z.object({
+      name: z
+        .string({ required_error: "Name is required" })
+        .trim()
+        .min(6, { message: "Name Must be 6 or more characters" }),
+      price: z
+        .number({ required_error: "Price is required" })
+        .positive({ message: "Price, must be positive" }),
+    });
 
-    if(!name) {
-        throw new AppError("O nome do produto ainda não foi cadastrado, cadastre o nome do produto antes de prosseguir!")
-    }
-    if (name.trim().length < 6) {
-        throw new AppError("O nome do poduto tem que ter pelomenos 6 caracteries")
-    }
-    if(!price) {
-        throw new AppError("O preço do produto  ainda não foi cadastrado, cadastre o preço do produto antes de prosseguir!")
-    }
-    if(price.length < 0){
-        throw new AppError("O preço do produto nao pode ser menor que 0")
-    }
+    const { name, price } = bodySchema.parse(req.body);
 
+    // if(!name) {
+    //     throw new AppError("O nome do produto ainda não foi cadastrado, cadastre o nome do produto antes de prosseguir!")
+    // }
+    // if (name.trim().length < 6) {
+    //     throw new AppError("O nome do poduto tem que ter pelomenos 6 caracteries")
+    // }
+
+    // if(!price) {
+    //     throw new AppError("O preço do produto  ainda não foi cadastrado, cadastre o preço do produto antes de prosseguir!")
+    // }
+    // if(price.length < 0){
+    //     throw new AppError("O preço do produto nao pode ser menor que 0")
+    // }
 
     res.status(201).json({ name, price, user_id: req.user_id });
   }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Response, Request } from "express";
+import { ZodError } from "zod";
+
 import { routes } from "./routes";
-import { AppError } from "./utils/app-Error";
+import { AppError } from "./utils/app-error";
 
 const PORT = 3333;
 
@@ -13,6 +15,12 @@ app.use(routes);
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .json({ message: "Validation Error", issues: error.format() });
   }
 
   res.status(500).json({ message: error.message });
